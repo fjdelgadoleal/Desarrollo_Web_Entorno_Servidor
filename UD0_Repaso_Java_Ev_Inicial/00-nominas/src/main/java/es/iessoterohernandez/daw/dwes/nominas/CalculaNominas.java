000986b7be4ed7a40125805b4ae9c2069bb6773a
fjdelgadoleal <fjdelgadoleal@yahoo.es>
2021-10-04 01:16:36 +0200

    Parte 2 finalizada con errores

--- UD0_Repaso_Java_Ev_Inicial/00-nominas/src/main/java/es/iessoterohernandez/daw/dwes/nominas/CalculaNominas.java
+++ UD0_Repaso_Java_Ev_Inicial/00-nominas/src/main/java/es/iessoterohernandez/daw/dwes/nominas/CalculaNominas.java
@@ -1,5 +1,4 @@
 package es.iessoterohernandez.daw.dwes.nominas;
-
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -17,6 +16,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 import es.iessoterohernandez.daw.dwes.nominas.laboral.ConexionBD;
 import es.iessoterohernandez.daw.dwes.nominas.laboral.DatosNoCorrectosException;
@@ -30,10 +30,10 @@ import es.iessoterohernandez.daw.dwes.nominas.laboral.Nomina;
  */
 public class CalculaNominas 
 {
-//    /**
-//     * Método main correspondiente a la parte 1 del ejercicio
-//     * @param args
-//     */
+    /**
+     * Método main correspondiente a la parte 1 del ejercicio
+     * @param args
+     */
 //	public static void main( String[] args )
 //    {
 //        try {
@@ -55,10 +55,10 @@ public class CalculaNominas
 //    	System.out.println(e2.imprime() + " - Sueldo="+ Nomina.sueldo(e2));
 //    }
 	
-//    /**
-//     * Método main correspondiente a la parte 2 del ejercicio leyendo y escribiendo en ficheros
-//     * @param args
-//     */
+    /**
+     * Método main correspondiente a la parte 2 del ejercicio leyendo y escribiendo en ficheros
+     * @param args
+     */
 //	public static void main( String[] args )
 //    {
 //        try {
@@ -187,9 +187,221 @@ public class CalculaNominas
 	  * Método main correspondiente a la parte 2 del ejercicio: Métodos "altaEmpleados" 
 	  * @param args
 	  */
+//	public static void main( String[] args )
+//    {
+//        EmpleadosDAO.altaEmpleado("empleadosNuevos.txt");
+//    }
+	
+	/**
+	  * Método main correspondiente a la parte 2 del ejercicio: Menú de opciones 
+	  * @param args
+	  */
 	public static void main( String[] args )
-    {
-        EmpleadosDAO.altaEmpleado("empleadosNuevos.txt");
-    }
+   {
+		boolean salir = false;		
+		
+		Scanner numeros = new Scanner(System.in);
+		Scanner letras = new Scanner(System.in);
+		String dni;
+				
+		while (!salir) {
+		
+			System.out.println("1. Ver todos los empleados \n"
+					+ "2. Ver salario de un empleado especifico \n"
+					+ "3. Submenu de edicion \n"
+					+ "4. Recalcular y actualizar sueldo de un empleado \n"
+					+ "5. Recalcular y actualizar sueldos de todos los empleados \n"
+					+ "6. Realizar copia de seguridad de la BBDD en fichero \n"
+					+ "7. Alta de nuevos empleados \n"
+					+ "8. Dar de baja empleado \n"
+					+ "9. Cargar backup-empleados.txt a la base de datos \n"
+					+ "0. Salir ");
+			String menuprincipal = letras.nextLine();
+		
+			switch (menuprincipal) {
+			case "1": 
+				for(Empleado e : EmpleadosDAO.getEmpleados()) {
+					System.out.println(e.imprime());
+				}
+				break;
+			
+			case "2":
+				System.out.println("Introduce el DNI del empleado del que quieres ver el sueldo");
+				dni = letras.nextLine();
+				int sueldo=EmpleadosDAO.getSueldo(dni);
+				
+				if( sueldo > 0) {
+					System.out.println("El salario del empleado con DNI: "+dni+" es de "+sueldo+"€.");
+				}else{
+					System.out.println("El empleado con ese DNI no existe");
+				}
+				break;
+				
+			case "3":
+				System.out.println("Introduce el DNI del empleado a editar");
+				dni = letras.nextLine().toUpperCase();
+				
+				if(EmpleadosDAO.exists(dni)) {
+					System.out.println("Que campos deseas editar? \n"
+							+ "1. Nombre \n"
+							+ "2. Sexo \n"
+							+ "3. Categoria \n"
+							+ "4. Anyos \n"
+							+ "5. Todos los campos \n"
+							+ "Introduce cualquier otra tecla para salir.");
+					String opcioneditar = letras.nextLine();
+					
+					Empleado e = EmpleadosDAO.getEmpleado(dni);
+					
+					switch (opcioneditar) {
+					case "1":
+						System.out.println("Introduce el nuevo nombre");
+						e.nombre = letras.nextLine();
+						EmpleadosDAO.updateEmpleado(e);
+						break;
+					case "2":
+						System.out.println("Introduce el nuevo sexo F/M");
+						e.sexo = letras.nextLine().charAt(0);
+						EmpleadosDAO.updateEmpleado(e);
+						break;
+					case "3":
+						System.out.println("Introduce la nueva categoria");
+						try {
+							e.setCategoria(numeros.nextInt());
+							EmpleadosDAO.updateEmpleado(e);
+						}catch (DatosNoCorrectosException ex) {
+							ex.printStackTrace();
+						}
+						break;
+					case "4":
+						System.out.println("Introduce el nuevo anyo");
+						e.anyos = numeros.nextInt();
+						EmpleadosDAO.updateEmpleado(e);
+						break;
+					case "5":
+						System.out.println("Introduce el nuevo nombre");
+						e.nombre = letras.nextLine();
+						System.out.println("Introduce el nuevo sexo F/M");
+						e.sexo = letras.nextLine().charAt(0);
+						System.out.println("Introduce la nueva categoria");
+						try {
+							e.setCategoria(numeros.nextInt());
+						}catch (DatosNoCorrectosException ex) {
+							ex.printStackTrace();
+						}						
+						System.out.println("Introduce el nuevo anyo");
+						e.anyos = numeros.nextInt();
+						EmpleadosDAO.updateEmpleado(e);
+						break;
+					default:
+						System.out.println("OK");
+						break;
+					}
+					
+				}
+				
+				break;
+				
+			case "4":
+				
+				System.out.println("Introduce el DNI del empleado a recalcular el sueldo");
+				dni = letras.nextLine();
+				
+				if(EmpleadosDAO.exists(dni)) {
+					EmpleadosDAO.updateSueldo(dni);
+					System.out.println("Consulta enviada");
+				}else {
+					System.out.println("Empleado inexistente");
+				}
+								
+				break;
+				
+			case "5":
+				
+				for(Empleado e : EmpleadosDAO.getEmpleados()) {
+					EmpleadosDAO.updateSueldo(e.dni);
+				}
+				System.out.println("Consulta enviada");
+				
+				break;
+				
+			case "6":
+				
+				for(Empleado e : EmpleadosDAO.getEmpleados()) {
+					EmpleadosDAO.backup(e);
+				}
+				System.out.println("Hecho :)");
+				
+				break;
+				
+			case "7":
+				System.out.println("Alta manual o mediante fichero empleadosNuevos.txt? \n"
+						+ "1. Manual \n"
+						+ "2. Fichero \n");
+				String opcionesalta = letras.nextLine();
+				String nombre, dnie;
+				char sexo;
+				int categoria, anyos;
+				
+				switch (opcionesalta) {
+				case "1":
+					System.out.println("Introduce el nombre del empleado");
+					nombre = letras.nextLine();
+					System.out.println("Introduce el DNI del empleado");
+					dnie = letras.nextLine().trim().toUpperCase();
+					System.out.println("Introduce el Sexo del empelado F/M");
+					sexo = letras.nextLine().charAt(0);
+					System.out.println("Introduce la categoria del empleado");
+					categoria=numeros.nextInt();
+					System.out.println("Introduce los anyos trabajados del empleado");
+					anyos = numeros.nextInt();
+					
+					EmpleadosDAO.altaEmpleado(nombre, dnie, sexo, categoria, anyos);
+					System.out.println("Consulta Enviada");
+									
+					break;
+				
+				case "2":
+					System.out.println("Introduce el nombre del fichero con la información de los empleados");
+					EmpleadosDAO.altaEmpleado(letras.nextLine());
+					System.out.println("Consulta Enviada");
+					break;
+
+				default:
+					System.out.println("Operación cancelada");
+					break;
+				}
+				
+				break;
+				
+			case "8":
+				
+				System.out.println("Introduce el DNI del empleado a dar de baja");
+				dni = letras.nextLine();
+				EmpleadosDAO.bajaEmpleado(dni);
+				System.out.println("Consulta enviada");
+				
+				break;
+				
+			case "9":
+				
+				EmpleadosDAO.altaEmpleado("backup-empleados.txt");
+				System.out.println("Hecho :)");
+				
+				break;
+				
+			case "0":
+				
+				System.out.println("Hasta otra :)");
+				EmpleadosDAO.cerrarConexion();
+				salir=true;
+				break;
+			}
+		
+		};
+			
+		letras.close();
+		numeros.close();
+	}
    
 }
--- UD0_Repaso_Java_Ev_Inicial/00-nominas/src/main/java/es/iessoterohernandez/daw/dwes/nominas/laboral/EmpleadosDAO.java
+++ UD0_Repaso_Java_Ev_Inicial/00-nominas/src/main/java/es/iessoterohernandez/daw/dwes/nominas/laboral/EmpleadosDAO.java
@@ -4,9 +4,11 @@
 package es.iessoterohernandez.daw.dwes.nominas.laboral;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 /**
  * @author fjdl
@@ -74,19 +76,29 @@ public class EmpleadosDAO {
         try {    
 	        Statement st=con.createStatement();
 			//Insertamos el empleado
-	        st.execute("insert into Empleados(nombre,dni,sexo,categoria,anyos) values ('"+e.nombre+"','"+e.dni+"','"+e.sexo+"','"+e.getCategoria()+"','"+e.anyos);
+	        st.execute("insert into Empleados(nombre,dni,sexo,categoria,anyos) values ('"+e.nombre+"','"+e.dni+"','"+e.sexo+"','"+e.getCategoria()+"','"+e.anyos+"')");
 	        //Insertamos el sueldo del empleado
 			st.execute("insert into nominas(dni, sueldo) values ('"+e.dni+"','"+Nomina.sueldo(e)+"')");
+
+			//hacemos el backup del punto 4
+			backup(e);
+			
         }catch (SQLException ex) {
 	    	  ex.printStackTrace();
 	    }
 	}
 	
+	public static void altaEmpleado(String nombre, String dni, char sexo, int categoria, int anyos) {
+		try {
+			Empleado e=new Empleado(nombre, dni, sexo, categoria, anyos);
+			altaEmpleado(e);
+		}catch (DatosNoCorrectosException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	
 	public static void altaEmpleado(String nombreFichero) {
-		//Conexón a la base de datos
-        Connection con = ConexionBD.getConnection();
-  
-        //Variables
         String line;;
         Empleado e;
         String []datosEmp;
@@ -97,8 +109,6 @@ public class EmpleadosDAO {
             FileReader fr = new FileReader(fentrada);        
             BufferedReader br = new BufferedReader(fr);
             
-	        Statement st=con.createStatement();
-
 	        while (br.ready()) {
 	        	//Leemos línea por línea el fichero de entrada
 	        	line = br.readLine();
@@ -107,22 +117,161 @@ public class EmpleadosDAO {
 	        		e = new Empleado(datosEmp[0], datosEmp[1], datosEmp[2].toCharArray()[0]);
 	        	else //en el caso de que vayan todos los datos
 	        		e = new Empleado(datosEmp[0], datosEmp[1], datosEmp[2].toCharArray()[0], Integer.parseInt(datosEmp[3]), Integer.parseInt(datosEmp[4]));
-
-				//Insertamos el empleado
-		        st.execute("insert into Empleados values ('"+e.nombre+"','"+e.dni+"','"+e.sexo+"',"+e.getCategoria()+","+e.anyos);
-		        //Insertamos el sueldo del empleado
-				st.execute("insert into nominas values ('"+e.dni+"',"+Nomina.sueldo(e)+")");
-				
-	        }	
+	        	altaEmpleado(e);	
+	        }
+	        br.close();
         }catch (FileNotFoundException ex){
         	ex.printStackTrace();
-        }catch (SQLException ex) {
-	    	ex.printStackTrace();
 	    }catch (IOException ex) {
 	    	ex.printStackTrace();
 	    }catch (DatosNoCorrectosException ex) {
 	    	ex.printStackTrace();
 	    }
+	}
+	
+	
+	/**
+	 * @param dni
+	 * @return
+	 */
+	public static int getSueldo(String dni) {
+		//Conexón a la base de datos
+        Connection con = ConexionBD.getConnection();
+        int sueldo=-1;
+        
+        try {    
+	        Statement st=con.createStatement();
+	        ResultSet rs=st.executeQuery("select sueldo from nominas where dni='"+dni+"'");
+	        
+	        if (rs!=null)
+				while (rs.next()) {
+					sueldo = rs.getInt(1);
+				}
+        }catch (SQLException ex) {
+        	ex.printStackTrace();
+		}
+        
+		return sueldo;
+	}
+	
+	/**
+	 * @param dni
+	 * @return
+	 */
+	public static boolean exists (String dni) {
+		//Conexón a la base de datos
+        Connection con = ConexionBD.getConnection();
+        boolean exists = false;
+        
+        try {    
+	        Statement st=con.createStatement();
+	        ResultSet rs=st.executeQuery("select count(*) from empleados where dni='"+dni+"'");
+	        
+	        if (rs!=null)
+				exists=true;
+        }catch (SQLException ex) {
+        	ex.printStackTrace();
+		}
+        
+		return exists;
+	}
+	
+	public static Empleado getEmpleado(String dni) {
+		//Conexón a la base de datos
+        Connection con = ConexionBD.getConnection();
+        Empleado e=null;
+	        
+        try {    
+	        Statement st=con.createStatement();
+	        ResultSet rs=st.executeQuery("select nombre, dni, sexo, categoria, anyos from empleados");
+	        
+	        //Solo puede haber un empleado con el mismo dni
+			while (rs.next()) {
+				e = new Empleado(rs.getString(1), rs.getString(2),  rs.getString(3).toCharArray()[0], rs.getInt(4), rs.getInt(5));
+			}
+        }catch (SQLException ex) {
+        	ex.printStackTrace();
+    	}catch (DatosNoCorrectosException ex) {
+    		ex.printStackTrace();
+		}
+        
+		return e;
+	}
+	
+	public static void updateEmpleado(Empleado e) {
+		//Conexón a la base de datos
+        Connection con = ConexionBD.getConnection();
+	        
+        try {    
+	        Statement st=con.createStatement();
+	        System.out.println("update empleados e set e.nombre='"+e.nombre+"' and e.sexo='"+e.sexo+"' and e.categoria="+e.getCategoria()+" and e.anyos="+e.anyos+" where e.dni='"+e.dni+"'");
+			//actualizamos el empleado (todos los campos, independientemente del que se haya cambiado
+	        st.execute("update empleados e set e.nombre='"+e.nombre+"' and e.sexo='"+e.sexo+"' and e.categoria="+e.getCategoria()+" and e.anyos="+e.anyos+" where e.dni='"+e.dni+"'");
+	        //actualizamos el sueldo del empleado por si ha cambiado su categoría
+			st.execute("update nominas set sueldo="+Nomina.sueldo(e)+" where dni='"+e.dni+"'");
+		
+        }catch (SQLException ex) {
+	    	  ex.printStackTrace();
+	    }
+	}
+	
+	public static void updateSueldo(String dni) {
+		//Conexón a la base de datos
+        Connection con = ConexionBD.getConnection();
+	        
+        try {    
+	        Statement st=con.createStatement();
+			//actualizamos el sueldo del empleado por si ha cambiado su categoría
+			st.execute("update nominas set sueldo="+Nomina.sueldo(getEmpleado(dni))+" where dni='"+dni+"'");
+		
+        }catch (SQLException ex) {
+	    	  ex.printStackTrace();
+	    }
+	}
+	
+	public static void bajaEmpleado(String dni) {
+		//Conexón a la base de datos
+        Connection con = ConexionBD.getConnection();
+	        
+        try {    
+	        Statement st=con.createStatement();
+			//Insertamos el empleado
+	        st.execute("delete from empleados e where e.dni = '"+dni+"'"); //Debería borrar en cascada	
+        }catch (SQLException ex) {
+	    	  ex.printStackTrace();
+	    }
+	}
+	
+	
+	/**
+	 * @param e
+	 */
+	public static void cerrarConexion() {
+        ConexionBD.close();
+	}
+		
+		
+	/**
+	 * @param e
+	 */
+	public static void backup(Empleado e) {
+        //Fichero de texto de salida para actualizar el de entrada
+    	File fbackup = new File("backup-empleados.txt");
+    	try {
+	        FileWriter fw = new FileWriter(fbackup.getAbsoluteFile(),true);
+	        BufferedWriter bw = new BufferedWriter(fw);
+	        
+	       	//Actualizamos el fichero de entrada, incluyendo el salario en el mismo fichero de backup
+	       	bw.write(e.nombre+";"+e.dni+";"+e.sexo+";"+e.getCategoria()+";"+e.anyos+";"+Nomina.sueldo(e)+'\n');
+	        
+	        //Cerramos todos los búferes
+	        bw.close();
+	        fw.close();
+    	}catch (FileNotFoundException ex) {
+    		ex.printStackTrace();
+    	}catch (IOException ex) {
+    		ex.printStackTrace();
+    	}
 	}
 
 }
